048d8097256ee103e113f3d693cd2c894039dc83
Aliskandar Gurbanov <98799140+aleksgurbanov@users.noreply.github.com>
2022-02-16 09:49:31 -0800

    Add files via upload

---
+++ CharArrayfinder/CharArrayfinder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CharArrayfinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = {"yoxlama", "bir", "bizon", "redbul"};
+            char yoxlayan = 'b';
+            foreach (string item in Chararrayfinder(arr,yoxlayan))
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static string[] Chararrayfinder(string[] arr, char a)
+        {
+            string[] netice = new string[0];
+
+            foreach (string item in arr)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (item[i] == a)
+                    {
+                        
+                        Array.Resize(ref netice, netice.Length + 1);
+
+                        
+                        netice[netice.Length - 1] = item;
+                        break;
+                    }
+
+                }
+            }
+
+            return netice;
+        }
+
+    }
+}
